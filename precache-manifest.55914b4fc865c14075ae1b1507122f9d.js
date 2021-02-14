49636709a5eb56e65fcbba4b094a98b54e021962
AwaisIsane <awaisane@gmail.com>
2021-02-14 23:55:35 +0530

    Updates

---
+++ precache-manifest.55914b4fc865c14075ae1b1507122f9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975cccdde5063e36a3ebc67688675d27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceba96c07e538de83278",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "44f9fb2ad6c07f25039f",
+    "url": "/static/js/2.0962ac68.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/static/js/2.0962ac68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceba96c07e538de83278",
+    "url": "/static/js/main.d5b7851a.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
